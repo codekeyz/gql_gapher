76b6be668364869ad77716d2713fc74e7bbafde8
Chima Precious <ositachima.co@gmail.com>
2023-11-10 20:56:37 +0000

    Depend purely on dart

--- test/parser_test.dart
+++ test/parser_test.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 import 'package:code_builder/code_builder.dart';
-import 'package:flutter_test/flutter_test.dart';
+import 'package:test/test.dart';
 
 import 'package:gql_codegen/src/parser.dart';
 import 'package:gql_codegen/src/data.dart';
