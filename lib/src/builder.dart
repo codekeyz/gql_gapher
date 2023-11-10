7275c01682dc24ba5e7fbda47f71592498019646
Chima Precious <ositachima.co@gmail.com>
2023-11-10 19:46:01 +0000

    Use buildStep ✅ to read imports & add support for relative paths

--- lib/src/builder.dart
+++ lib/src/builder.dart
@@ -12,7 +12,11 @@ class GraphqlBuilder implements Builder {
     final resultFile = inputId.changeExtension('.g.dart');
     final fileContent = await buildStep.readAsString(inputId);
 
-    final operations = await parseGqlString(fileContent);
+    final sources = await Future.wait(getImportLines(fileContent)
+        .map((line) => resolvePath(line, inputId.path))
+        .map((e) => buildStep.readAsString(AssetId(inputId.package, e!))));
+
+    final operations = await parseGqlString(fileContent, sources: sources);
     if (operations.isEmpty) return;
 
     final classes = await Future.wait(operations.map(getClassDefinition));
@@ -22,6 +26,26 @@ class GraphqlBuilder implements Builder {
       resultFile,
       DartFormatter().format('${library.accept(DartEmitter())}'),
     );
+  }
+
+  String? resolvePath(String relativePath, String filePath) {
+    final parts = filePath.split('/')..removeLast();
+    return chopPath(parts.join('/'), relativePath);
+  }
+
+  String? chopPath(String path, String relativePath) {
+    final segments = relativePath.split('/');
+    final resultingPath = path.split('/');
+    while (segments.isNotEmpty) {
+      if (resultingPath.isEmpty) return null;
+      final seg = segments.removeAt(0);
+      if (RegExp(r'^\.\.?').hasMatch(seg)) {
+        if (seg.length == 2) resultingPath.removeLast();
+        continue;
+      }
+      resultingPath.add(seg);
+    }
+    return resultingPath.join('/');
   }
 
   @override
